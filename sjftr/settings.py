e893988402e786781d67700069a4d303a77f100e
Anvar <101347819+anvaraxadjonov1802@users.noreply.github.com>
2025-08-17 22:37:38 +0500

    Update settings.py

--- sjftr/settings.py
+++ sjftr/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z9f^54w%65%qrco9irn*s-zw#=#s9l2(!*hovm(&c^$zc78pn1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
